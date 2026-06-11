5c5c1df765d2ccfce2833e93e0c30f7c0fc707ae
oygurd <nirmula3d@gmail.com>
2026-06-11 18:14:39 +0300

    implementing an upgrade system

--- Assets/_scripts/Enemies/Ranged Enemy/ProjectileBehaviour.cs
+++ Assets/_scripts/Enemies/Ranged Enemy/ProjectileBehaviour.cs
@@ -17,6 +17,9 @@ public class ProjectileBehaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.
+        if (other.gameObject.CompareTag("Player")) //these normal projectiles will have the same tag as the enemy, unless a new enemy with projectiles is made and a scriptable object will be used instead
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/_scripts/Upgrades/UpgradesScriptableObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UpgradesScriptableObject", menuName = "Scriptable Objects/UpgradesScriptableObject")]
+public class UpgradesScriptableObject : ScriptableObject
+{
+    [Header("Upgrade Name")]
+    public string upgradeName;
+    public string upgradeDescription;
+    public Sprite upgradeIcon;
+    
+    [Header("Stats")]
+    public int damageBonus;
+    public int healthBonus;
+    public int speedBonus;
+    
+}
