ae0edf04f0e44e68a46a2279390209d45135bcfc
oygurd <nirmula3d@gmail.com>
2026-07-22 22:54:42 +0300

    added esc menu

--- Assets/_scripts/_UI/Escape Menu/EscapeMenueManager.cs
+++ Assets/_scripts/_UI/Escape Menu/EscapeMenueManager.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
 public class EscapeMenueManager : MonoBehaviour
 {
+    InputAction escapeAction;
+    public GameObject escapeMenu;
+    bool willPause;
+
+    private int changePause;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //escapeMenu = GameObject.Find("escape menue UI holder");
+        escapeAction = InputSystem.actions.FindAction("ESC");
+        changePause = Convert.ToInt32(willPause);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (escapeAction.triggered)
+        {
+            willPause = !willPause;
+            if (willPause)
+            {
+                EscContinue(0);
+                escapeMenu.SetActive(true);
+                Cursor.visible = true;
+            }
+            else
+            {
+                EscContinue(1);
+                escapeMenu.SetActive(false);
+                Cursor.visible = false;
+            }
+            
+        }
+    }
+
+    public void EscContinue(int scale)
+    {
+        Time.timeScale = scale;
+    }
+
+    public void Continue()
+    {
+        willPause = !willPause;
+        Time.timeScale = 1;
+        escapeMenu.SetActive(false);
+        Cursor.visible = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
